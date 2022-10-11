945a883a6927fa24c0672fe96afae8c3e60cecd7
Vladimir RM <shvdrummer@gmail.com>
2022-10-11 16:55:33 +0300

    ad task list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import React from "react";
 
-
-
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [
+        {
+          id: 0,
+          title: "Create todo React app",
+          done: false,
+        },
+        {
+          id: 1,
+          title: "Make a video about it",
+          done: true,
+        },
+        {
+          id: 2,
+          title: "Create simple todo app",
+          done: false,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="top">Active tasks: {}</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
